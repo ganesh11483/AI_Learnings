f3c0e4cac959703e26881a12405d19f2734566f0
Prasad2 <ganesh.prasad2@dell.com>
2026-06-28 00:59:56 +0530

    Fix import error in Layout.jsx
    - Fixed useState import from react-router-dom to react
    - This was causing blank page error on frontend

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,4 +1,5 @@
-import { Outlet, Link, useLocation, useState } from 'react-router-dom'
+import { Outlet, Link, useLocation } from 'react-router-dom'
+import { useState } from 'react'
 import { useAuth } from '../hooks/useAuth'
 import { useTheme } from '../contexts/ThemeContext'
 import { 
